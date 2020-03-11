f702c72275279ae78def9af9365ce7252501e754
Rinat <khusainovrm@yandex.ru>
2020-03-11 17:56:35 +0100

    smth strange

--- mysite/apps/blog/urls.py
+++ mysite/apps/blog/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 
 from . import views
 urlpatterns = [
-    path('', views.post_list, name = '-post_list')
+    path('', views.post_list, name = 'post_list'),
+    path ('home', views.home, name='home')
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- mysite/apps/blog/views.py
+++ mysite/apps/blog/views.py
@@ -5,3 +5,7 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.order_by("created_date")
     return render(request, 'blog/post_list.html', {"posts" : posts})
+
+def home(request):
+    pops = Post.objects.all()
+    return render(request, 'blog/base.html', {"posts" : pops})
