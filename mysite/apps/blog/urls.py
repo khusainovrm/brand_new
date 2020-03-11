381387ba42022b37dc34d5b68653ab25013e57fb
Rinat <khusainovrm@yandex.ru>
2020-03-11 14:59:02 +0100

    Applying CSS for site

--- mysite/apps/blog/urls.py
+++ mysite/apps/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 urlpatterns = [
     path('', views.post_list, name = 'post_list')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
