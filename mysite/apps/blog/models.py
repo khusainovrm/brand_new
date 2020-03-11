bfaee0ce5fca281e9905b7832fa8c92dbea3165b
Rinat <khusainovrm@yandex.ru>
2020-03-11 14:05:45 +0100

    Inserted query from base as text for arcticles

--- mysite/apps/blog/models.py
+++ mysite/apps/blog/models.py
@@ -1,14 +1,16 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    title = models.CharField("Title", max_length=200)
+    text = models.TextField("Text")
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    me = User.objects.get (username='rinat')
 
     def publish(self):
         self.published_date = timezone.now()
@@ -16,3 +18,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ["-created_date"]
--- mysite/apps/blog/views.py
+++ mysite/apps/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by("-created_date")
+    return render(request, 'blog/post_list.html', {"posts" : posts})
