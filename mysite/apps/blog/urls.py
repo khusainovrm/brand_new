5bef5f0705a4b0b0614894fb0b2c640c91b1b9b9
Rinat <khusainovrm@yandex.ru>
2020-03-13 08:57:13 +0100

    Final version from dg

--- mysite/apps/blog/urls.py
+++ mysite/apps/blog/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
-app_name="blog"
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path ('', views.post_list, name='main_list'),
-    path('post/<int:id>/', views.post_detail, name='post_detail_name'),
-    path('create/', views.post_create_view, name ='create'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path ('post/new/', views.post_new, name='post_new'),
+    path ('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ] + static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- mysite/apps/blog/views.py
+++ mysite/apps/blog/views.py
@@ -8,22 +8,34 @@ def post_list(request):
     posts = Post.objects.order_by("-published_date")
     return render(request, 'blog/post_list.html', {"posts" : posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id)
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-"""def post_create(request):
-    posts = Post.objects.order_by("-published_date")
-    return render(request, 'blog/post_create.html', {"posts" : posts})"""
-
-def post_create_view(request):
-    form = PostForm(request.POST or None)
-    if form.is_valid():
-        post = form.save (commit=False)
-        post.author = request.user
-        post.published_date = timezone.now ()
-        post.save ()
-        return redirect ('blog:post_detail_name', id=post.id)
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
     else:
-        form = PostForm ()
-    return render(request, "blog/post_create.html", {'form': form})
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
